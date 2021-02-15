ecb48f0a230522f8f65b76e8e12aacd2461b3139
alce65 <alejandro.cerezo@evosoft.es>
2021-02-15 20:00:23 +0100

    Angular inicial

---
+++ curso/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vwn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Curso-Viewnext';
+  logo ='../assets/logo.svg'
+}
