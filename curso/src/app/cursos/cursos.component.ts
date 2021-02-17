6dab68739ed11bd1920c6a4665c38f7860bb4365
alce65 <alejandro.cerezo@evosoft.es>
2021-02-17 20:04:17 +0100

    formularios

---
+++ curso/src/app/cursos/cursos.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vwn-cursos',
+  template: `
+    <div class="row">
+      <vwn-form-td class="col-6"></vwn-form-td>
+    </div>
+  `,
+  styles: [
+  ]
+})
+export class CursosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ curso/src/app/cursos/form-td/form-td.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Alumno, Curso, Turno } from 'src/app/models/alumno';
+import { CURSOS, TURNOS } from 'src/app/models/maestros.data';
+
+@Component({
+  selector: 'vwn-form-td',
+  templateUrl: './form-td.component.html',
+  styleUrls: ['./form-td.component.scss']
+})
+export class FormTdComponent implements OnInit {
+  turnos: Array<Turno>
+  cursos: Array<Curso>
+  alumno: Alumno;
+  @ViewChild('formAlumnos', {static: true}) form: NgForm
+  constructor() { }
+
+  ngOnInit(): void {
+    this.cursos = CURSOS
+    this.turnos = TURNOS
+    console.log(this.form)
+  }
+
+  enviar() {
+    this.alumno = this.form.value
+    console.log(this.alumno)
+  }
+
+}
